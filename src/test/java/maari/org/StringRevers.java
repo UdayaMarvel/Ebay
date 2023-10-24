ed5d55d3bf6f16aa1ae2d084723ce10ca5da4d7a
marimuhil <maricdm777@gmail.com>
2023-10-24 14:02:20 +0530

    reversstring

---
+++ src/test/java/maari/org/StringRevers.java
@@ -0,0 +1,14 @@
+package maari.org;
+
+public class StringRevers {
+   public static void main(String[] args) {
+	    String s="mariyappan";
+	    String s1="";
+	    for(int i=s.length()-1;i>=0;i--) {
+	    	char c=s.charAt(i);
+	    	s1=s1+c;
+	    	
+	    }
+	    System.out.println(s1);
+}
+}
