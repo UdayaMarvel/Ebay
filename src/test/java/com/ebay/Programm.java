efc974f8974d78131a2fcb4c0f4485ffddb7f66f
Palanikumar93 <palanimech93@gmail.com>
2023-10-26 12:55:14 +0530

    programm

---
+++ src/test/java/com/ebay/Programm.java
@@ -0,0 +1,15 @@
+package com.ebay;
+
+public class Programm {
+    public static void main(String[] args) {
+		String s="palani";
+		String s1="";
+		for (int i=s.length()-1;i>=0;i--) {
+			char c=s.charAt(i);
+			 s1=s1+c;
+			 
+		}
+		System.out.println(s1);
+				
+	}
+}
